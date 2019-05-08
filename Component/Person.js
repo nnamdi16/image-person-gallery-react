c69a78801bcebf74b8071c7e06c4b3a3aa1bc8a5
nnamdi16 <nwabuokeinnamdi@gmail.com>
2019-05-08 14:31:33 +0100

    Create a person.js file for rendering a single person

---
+++ Component/Person.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import '../App.css';
+
+const Person = props => {
+	const { img, name, job } = props.person;
+	const { children } = props;
+	const url = `https://randomuser.me/api/portraits/thumb/men/${img}.jpg`;
+	return (
+		<div className="person">
+			<img src={url} alt="" />
+			<h4>{name}</h4>
+			<h4>{job}</h4>
+			{children}
+		</div>
+	);
+};
+
+export default Person;
