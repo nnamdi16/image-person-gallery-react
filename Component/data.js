6c6acd5f7320787bda2c389286a042de21192a02
nnamdi16 <nwabuokeinnamdi@gmail.com>
2019-05-08 14:23:59 +0100

    Database for creating the list of books

---
+++ Component/data.js
@@ -0,0 +1,31 @@
+const books = [
+	{
+		id: 1,
+		img: 'https://images-na.ssl-images-amazon.com/images/I/81h2gWPTYJL._AC_UL200_SR200,200_.jpg',
+		Title: 'Becoming',
+		Author: 'Michelle Obama'
+	},
+
+	{
+		id: 2,
+		img: 'https://images-na.ssl-images-amazon.com/images/I/51j5p18mJNL._SX330_BO1,204,203,200_.jpg',
+		Title: 'Where the Crawdads Sing ',
+		Author: 'Delia Owens'
+	},
+
+	{
+		id: 3,
+		img: 'https://images-na.ssl-images-amazon.com/images/I/91ibhD5nhUL._AC_UL200_SR200,200_.jpg',
+		Title: 'Girl, Wash Your Face: Stop Believing the Lies…',
+		Author: 'Rachel Hollis'
+	},
+
+	{
+		id: 4,
+		img: 'https://images-na.ssl-images-amazon.com/images/I/81WojUxbbFL._AC_UL200_SR200,200_.jpg',
+		Title: 'Educated: A Memoir',
+		Author: 'Tara Westover'
+	}
+];
+
+export default books;
